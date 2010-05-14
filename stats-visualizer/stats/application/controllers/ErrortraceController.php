fc8bc4d4932c650f7a25d3702937921af916ab5f
Eugene Novikov <joker@joker.joker.igroup.ispras.ru>
2010-05-14 12:46:41 +0400

    Add links and processing for error traces.

--- stats-visualizer/stats/application/controllers/ErrortraceController.php
+++ stats-visualizer/stats/application/controllers/ErrortraceController.php
@@ -2,8 +2,11 @@
 
 class ErrortraceController extends Zend_Controller_Action
 {
-	public $_errorTrace;
-	
+    protected $_dbName;
+    protected $_dbUser;
+    protected $_dbHost;
+    protected $_dbUserPassword;
+    	
     public function init()
     {
         /* Initialize action controller here */
@@ -11,9 +14,43 @@ class ErrortraceController extends Zend_Controller_Action
 
     public function indexAction()
     {
-		$ref = $this->_getParam('ref');
-		print($ref);
-		print("HIE");
+		$driverId = $this->_getParam('driver id');
+		$kernelId = $this->_getParam('kernel id');
+		$modelId = $this->_getParam('model id');
+		$toolsetId = $this->_getParam('toolset id');
+		$scenarioId = $this->_getParam('scenario id');				
+        $request = $this->getRequest();
+        
+        $form    = new Application_Form_Db();
+        if ($this->getRequest()->isPost()) {
+            if ($form->isValid($request->getPost())) 
+            {
+				$formValues = $form->getValues();
+
+				$this->_dbName = $formValues['name'];
+				$this->_dbUser = $formValues['user'];
+				$this->_dbHost = $formValues['host'];
+				$this->_dbUserPassword = $formValues['user password'];
+
+               // return $this->_helper->redirector('index');
+            }
+        }
+
+        $this->view->form = $form;		
+
+		if (!empty($this->_dbHost))
+		{
+	        /* Create the database adapter. */	
+            $db = new Zend_Db_Adapter_Pdo_Mysql(array(
+                'host'     => $this->_dbHost,
+                'username' => $this->_dbUser,
+                'password' => $this->_dbUserPassword,
+                'dbname'   => $this->_dbName
+            ));
+		
+            $statistics = new Application_Model_StatsMapper(array('db' => $db));
+            $this->view->entries = array('stats' => $statistics->getErrorTrace($driverId, $kernelId, $modelId, $toolsetId, $scenarioId));
+	    }
     }
     
 
