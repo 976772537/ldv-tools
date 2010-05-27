448d481b707f8dc9a3d74b9742da8c484d1c75e9
shvedsky <shved@ispras.ru>
2010-05-27 14:17:10 +0400

    LDV-manager is now installed

--- ldv-manager/ldv-manager.mk
+++ ldv-manager/ldv-manager.mk
@@ -1,6 +1,11 @@
 export SHELL=/bin/bash
 
-Lib_dir:=$(dir $(MAKEFILE_LIST))
+ifeq ($(LDV_SRVHOME),)
+$(error LDV_SRVHOME is not set!)
+endif
+
+Lib_dir:=$(LDV_SRVHOME)/ldv-manager/mk/
+Script_dir:=$(LDV_SRVHOME)/ldv-manager/
 
 # Configuration variables
 # Include config only if it exists
@@ -23,6 +28,7 @@ Verifier=
 else
 Verifier=$(delim)$(RCV_VERIFIER)
 endif
+
 
 # Install dir should be absolutized
 LDV_INSTALL_DIR:=$(abspath $(LDV_INSTALL_DIR))
@@ -125,8 +131,8 @@ $$(WORK_DIR)/$(1)$(Verifier)/finished: $$(WORK_DIR)/$(1)$(Verifier)/checked
 	@# Add ancillary information to reports and post it to target directory
 	@echo $(call mkize,$(1))
 	@mkdir -p $$(dir $(RESULTS_DIR)/$$(call rmtr,$$(@D)).report.xml)
-	$(Lib_dir)report-fixup $$(@D)/report_after_ldv.xml $$(Tag) $$(Driver) $(if $(kernel_driver),kernel,external) $$(@D)/report_after_ldv.xml.source/ $$(@D) >$(TMP_DIR)/$(call mkize,$(1))$(Verifier).report.xml
-	$(Lib_dir)package $(TMP_DIR)/$(call mkize,$(1))$(Verifier).report.xml $(RESULTS_DIR)/$(call mkize,$(1))$(Verifier).pax -s '|^$(TMP_DIR)\/*||'
+	$(Script_dir)report-fixup $$(@D)/report_after_ldv.xml $$(Tag) $$(Driver) $(if $(kernel_driver),kernel,external) $$(@D)/report_after_ldv.xml.source/ $$(@D) >$(TMP_DIR)/$(call mkize,$(1))$(Verifier).report.xml
+	$(Script_dir)package $(TMP_DIR)/$(call mkize,$(1))$(Verifier).report.xml $(RESULTS_DIR)/$(call mkize,$(1))$(Verifier).pax -s '|^$(TMP_DIR)\/*||'
 	touch $$@
 endef
 
