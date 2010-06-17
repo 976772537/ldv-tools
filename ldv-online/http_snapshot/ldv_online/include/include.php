5943528df337ac24a355cfd4f622dda574ffb7e4
iceberg <iceberg@pamir.kazbek.ispras.ru>
2010-06-17 19:23:54 +0400

    remove old http snapshot

--- ldv-online/http_snapshot/ldv_online/include/include.php
+++
@@ -1,5 +0,0 @@
-<?php
-print "<link rel=\"stylesheet\" type=\"text/css\" href=\"ldv_online/include/ldv.css\" media=\"screen\" />";
-print "<script type=\"text/javascript\" src=\"ldv_online/include/jquery-1.4.2.min.js\"></script>";
-include_once("ldv_online/include/lsapi.php");
-?>
