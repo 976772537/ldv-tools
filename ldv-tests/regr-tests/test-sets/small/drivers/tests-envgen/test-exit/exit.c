30b03545a8b6733e4e395a8d986a2e79c6a09cfe
Vadim Mutilin <mutilin@ispras.ru>
2010-06-23 19:44:11 +0400

    tests for envgen, 29_1, 37_1, 43_1, 68_1, 77_1

---
+++ ldv-tests/regr-tests/test-sets/small/drivers/tests-envgen/test-exit/exit.c
@@ -0,0 +1,40 @@
+/** 
+ * The test checks that correct spin lock is safe on the models 39_1,39_2
+ */
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/spinlock.h>
+#include <linux/major.h>
+#include <linux/fs.h>
+
+static DEFINE_SPINLOCK(my_lock);
+
+static int misc_open(struct inode * inode, struct file * file);
+
+static const struct file_operations misc_fops = {
+        .owner          = THIS_MODULE,
+        .open           = misc_open,
+};
+
+static int misc_open(struct inode * inode, struct file * file)
+{
+	return 0;
+}
+
+static int __init my_init(void)
+{
+	return 0;
+}
+
+static void __exit my_exit(void)
+{
+	spin_lock(&my_lock);
+	spin_lock_irqsave(&my_lock, flags);
+}
+
+module_init(my_init);
+module_exit(my_exit);
+
+MODULE_LICENSE("Apache 2.0");
+MODULE_AUTHOR("LDV Project, Vedim Mutilin <mutilin@ispras.ru>");
+
---
+++ ldv-tests/regr-tests/test-sets/small/drivers/tests-model-37_1/test-nodelay/nodelay.c
@@ -0,0 +1,51 @@
+/** 
+ * The test checks that correct spin lock is safe on the models 39_1,39_2
+ */
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/major.h>
+#include <linux/fs.h>
+#include <linux/interrupt.h>
+
+struct my_device {
+	struct module mod;
+} mydev;
+
+static int misc_open(struct inode * inode, struct file * file);
+
+static const struct file_operations misc_fops = {
+        .owner          = THIS_MODULE,
+        .open           = misc_open,
+};
+
+
+static int misc_open(struct inode * inode, struct file * file)
+{
+	long mask;
+	int res;
+	mask = probe_irq_on();
+	res = probe_irq_off(mask);
+	
+	return 0;
+}
+
+static int __init my_init(void)
+{
+	if (register_chrdev(MISC_MAJOR,"misc",&misc_fops))
+		goto fail_register;
+	return 0;
+	
+fail_register:
+	return -1;
+}
+
+static void __exit my_exit(void)
+{
+}
+
+module_init(my_init);
+module_exit(my_exit);
+
+MODULE_LICENSE("Apache 2.0");
+MODULE_AUTHOR("LDV Project, Vedim Mutilin <mutilin@ispras.ru>");
+
