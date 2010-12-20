3f5e39378d1e6951cc57d2d4516cfbcf9379f116
shvedsky <shved@ispras.ru>
2010-12-20 21:21:24 +0300

    Add generic sender-to-cluster Ruby class

---
+++ cluster/sender.rb
@@ -0,0 +1,27 @@
+# Ruby interface to sending stuff to cluster
+#
+require 'rubygems'
+require 'nanite'
+
+class NaniteSender
+
+	attr_accessor :options
+	def initialize(opts = {})
+		@options = opts.dup
+		@options[:ping_time] = 1
+		@options[:root] = File.dirname(__FILE__)
+	end
+
+	def send(target,payload)
+		EM.run do
+			agent = Nanite::Agent.new({:host => 'localhost', :user => 'mapper', :pass => 'testing', :vhost => '/nanite', :format => :json, :initrb => File.join(@options[:root],'tester.rb')}.merge self.options)
+			agent.run
+
+			Nanite.push(target, payload, :selector => :ldv_selector)
+			EM.add_timer(1) { EM.stop }
+			#EM.stop
+		end
+	end
+
+end
+
