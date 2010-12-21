da8505b19e42ec2fc56e494443c8978f9a28d69a
shvedsky <shved@ispras.ru>
2010-12-21 22:07:57 +0300

    Made spawning processes in node asynchronous

--- cluster/actors/node.rb
+++ cluster/actors/node.rb
@@ -1,6 +1,11 @@
 require 'tempfile'
 require 'fileutils'
 
+def say_and_run(*args)
+	$stderr.write "Running: #{args.inspect}\n"
+	Kernel.system *args
+end
+
 class Ldvnode
 	include Nanite::Actor
 	expose :hello, :dscv, :ldv, :rcv
@@ -38,34 +43,41 @@ class Ldvnode
 	protected
 
 	def launch_child_job(job_type,task)
-		# Update local status
-		@status_update_mutex.synchronize do
-			if @status[job_type] > 0
-				@status[job_type] -= 1
-			else
-				# Send task back to queuer, and exit from the outer method
+		puts "Job #{job_type} accepted: #{task.inspect}."
+		# Whether we descided to send job back (local for closures created below)
+		send_job_back = false
+
+		child_op = proc do
+			# Update local status
+			@status_update_mutex.synchronize do
+				if @status[job_type] > 0
+					@status[job_type] -= 1
+				else
+					send_job_back = true
+				end
+			end
+			unless send_job_back
+				# Set key (which is transferred via an environment variable)
+				ENV['LDV_SPAWN_KEY'] = task['key']
+				spawn_child(job_type,task)
+			end
+		end
+
+		callback_op = proc do
+			if send_job_back
+				# If the job's to be sent back, don't touch statuses, just send
 				puts "A #{job_type} job rejected, sending back: #{task.inspect}"
 				Nanite.push("/ldvqueue/redo", task)
-				return
+			else
+				@status_update_mutex.synchronize do
+					@status[job_type] += 1
+				end
 			end
 		end
-		puts "Job #{job_type} accepted: #{task.inspect}"
-		begin
-			# Set key (which is transferred via an environment variable)
-			ENV['LDV_SPAWN_KEY'] = task['key']
 
-			spawn_child(job_type,task)
-		ensure
-			puts "Backing status"
-			@status_update_mutex.synchronize do
-				@status[job_type] += 1
-			end
-		end
-		puts "The #{job_type} job done"
-		# We sleep for some time, greater than ping time.  Server will update status if we use ldv_selector, however, that status should already be delievered.  Status is sent by ping, so we must be sure that ping succeeded before we send the actual result.
-		EM.add_timer(ping_time.call + 2) do
-			Nanite.push("/ldvqueue/result", task, :selector => :ldv_selector)
-		end
+		EM.defer child_op, callback_op
+
+		puts "The #{job_type} will run in the background, and will send results!"
 	end
 
 	Child_jobs = { :ldv => {:cmd=>'ldv-manager'},
@@ -89,7 +101,7 @@ class Ldvnode
 				# ldv-manager gets all it needs from environment.  However, we should create a directory for it, and work inside it.
 				puts "Creating #{workdir}"
 				Dir.chdir(FileUtils.mkdir_p(workdir)) do
-					unless Kernel.system('ldv-manager')
+					unless say_and_run('ldv-manager')
 						$stderr.puts "Failed to run ldv-manager..."
 					end
 				end
@@ -105,7 +117,7 @@ class Ldvnode
 					puts task['args']
 
 					ENV['WORK_DIR'] = workdir
-					Kernel.system('dscv',"--rawcmdfile=#{temp_file.path}")
+					say_and_run('dscv',"--rawcmdfile=#{temp_file.path}")
 				end
 			when :rcv then
 				puts "DSCV"
@@ -118,8 +130,9 @@ class Ldvnode
 					puts "Saved task to temporary file #{temp_file.path}"
 					puts task['args']
 
+					ENV['DSCV_HOME'] = @home
 					ENV['WORK_DIR'] = workdir
-					Kernel.system(File.join(@home,'dscv','rcv','blast'),"--rawcmdfile=#{temp_file.path}")
+					say_and_run(File.join(@home,'dscv','rcv','blast'),"--rawcmdfile=#{temp_file.path}")
 				end
 		end
 	end
