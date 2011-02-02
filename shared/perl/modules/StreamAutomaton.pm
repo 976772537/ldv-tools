a602a3bbc03ef7d8b64aa6e9d3f2c1f72fc60894
Alexander Strakh <cromlehg@gmail.com>
2011-02-02 16:45:05 +0300

    Ticklimit renamed to hanguplimit

--- shared/perl/modules/StreamAutomaton.pm
+++ shared/perl/modules/StreamAutomaton.pm
@@ -150,7 +150,7 @@ sub limits_check
 		my $l=shift or return undef;
 		$l=~/${idstr}TIMEOUT (\d+)/i and return {'LIMITS'=>'Time Limit Exceeded'};
 		$l=~/${idstr}MEM (\d+)/i and return {'LIMITS'=>'Memory Limit Exceeded'};
-		$l=~/${idstr}TICKS (\d+)/i and return {'LIMITS'=>'Ticks Limit Exceeded'};
+		$l=~/${idstr}HANGUP (\d+)/i and return {'LIMITS'=>'Hangup Detected'};
 		return undef;
 	}];
 }
