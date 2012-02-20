3cde4bcc6fecae825ce4b3ddf11d354c6295114b
Eugene Novikov <joker@ispras.ru>
2012-02-20 19:52:10 +0400

    Infrastructure doesn't use deprecated option --engine for ETV

--- stats-visualizer/vhosts/ldv-stats/application/models/KnowledgeBase.php
+++ stats-visualizer/vhosts/ldv-stats/application/models/KnowledgeBase.php
@@ -2,54 +2,5 @@
 
 class Application_Model_KnowledgeBase extends Application_Model_General
 {
-/*
-  protected $_errorTraceRaw;
-  protected $_sourceCodeFiles;
-  protected $_engine;
-  protected $_errorTrace;
 
-  public function setErrorTraceRaw($errorTrace)
-  {
-    $this->_errorTraceRaw = $errorTrace;
-    return $this;
-  }
-
-  public function getErrorTraceRaw()
-  {
-    return $this->_errorTraceRaw;
-  }
-
-  public function setErrorTrace($errorTrace)
-  {
-    $this->_errorTrace = $errorTrace;
-    return $this;
-  }
-
-  public function getErrorTrace()
-  {
-    return $this->_errorTrace;
-  }
-
-  public function setSourceCodeFiles($sourceCodeFiles)
-  {
-    $this->_sourceCodeFiles = $sourceCodeFiles;
-    return $this;
-  }
-
-  public function getSourceCodeFiles()
-  {
-    return $this->_sourceCodeFiles;
-  }
-
-  public function setEngine($engine)
-  {
-    $this->_engine = $engine;
-    return $this;
-  }
-
-  public function getEngine()
-  {
-    return $this->_engine;
-  }
-*/
 }
