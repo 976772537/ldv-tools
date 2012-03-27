7422c1c464d294ee1ad98c869648b179c23f2bad
Eugene Novikov <joker@ispras.ru>
2012-03-27 17:30:08 +0400

    Insert copyright notices for error trace visualizer source code

--- shared/perl/modules/ETV/Parser.pm
+++ shared/perl/modules/ETV/Parser.pm
@@ -489,7 +489,7 @@ sub _Parse {
 #End of include--------------------------------------------------
 
 
-#line 1 "shared/perl/modules/ETV/Parser.ym"
+#line 18 "shared/perl/modules/ETV/Parser.ym"
 
 use English;
 
@@ -641,7 +641,7 @@ sub new {
 	[#Rule 2
 		 'input', 2,
 sub
-#line 8 "shared/perl/modules/ETV/Parser.ym"
+#line 25 "shared/perl/modules/ETV/Parser.ym"
 {
   my $parser = $ARG[0];
 
@@ -654,13 +654,13 @@ sub
 	[#Rule 3
 		 'str', 2,
 sub
-#line 17 "shared/perl/modules/ETV/Parser.ym"
+#line 34 "shared/perl/modules/ETV/Parser.ym"
 { return $ARG[1]; }
 	],
 	[#Rule 4
 		 'node', 7,
 sub
-#line 20 "shared/perl/modules/ETV/Parser.ym"
+#line 37 "shared/perl/modules/ETV/Parser.ym"
 {
   my $parser = $ARG[0];
 
@@ -738,7 +738,7 @@ sub
 	[#Rule 6
 		 'line', 1,
 sub
-#line 92 "shared/perl/modules/ETV/Parser.ym"
+#line 109 "shared/perl/modules/ETV/Parser.ym"
 { return $ARG[1]; }
 	],
 	[#Rule 7
@@ -747,7 +747,7 @@ sub
 	[#Rule 8
 		 'file', 1,
 sub
-#line 95 "shared/perl/modules/ETV/Parser.ym"
+#line 112 "shared/perl/modules/ETV/Parser.ym"
 { return $ARG[1]; }
 	],
 	[#Rule 9
@@ -756,7 +756,7 @@ sub
 	[#Rule 10
 		 'type', 1,
 sub
-#line 98 "shared/perl/modules/ETV/Parser.ym"
+#line 115 "shared/perl/modules/ETV/Parser.ym"
 { return $ARG[1]; }
 	],
 	[#Rule 11
@@ -765,7 +765,7 @@ sub
 	[#Rule 12
 		 'kind', 1,
 sub
-#line 101 "shared/perl/modules/ETV/Parser.ym"
+#line 118 "shared/perl/modules/ETV/Parser.ym"
 { return $ARG[1]; }
 	],
 	[#Rule 13
@@ -774,7 +774,7 @@ sub
 	[#Rule 14
 		 'skip_reason', 1,
 sub
-#line 104 "shared/perl/modules/ETV/Parser.ym"
+#line 121 "shared/perl/modules/ETV/Parser.ym"
 { return $ARG[1]; }
 	],
 	[#Rule 15
@@ -783,25 +783,25 @@ sub
 	[#Rule 16
 		 'formal_arg_names_list', 1,
 sub
-#line 107 "shared/perl/modules/ETV/Parser.ym"
+#line 124 "shared/perl/modules/ETV/Parser.ym"
 { return $ARG[1]; }
 	],
 	[#Rule 17
 		 'formal_arg_names', 2,
 sub
-#line 109 "shared/perl/modules/ETV/Parser.ym"
+#line 126 "shared/perl/modules/ETV/Parser.ym"
 { my @formal_arg_names = (@{$ARG[1]}, $ARG[2]); return \@formal_arg_names; }
 	],
 	[#Rule 18
 		 'formal_arg_names', 1,
 sub
-#line 110 "shared/perl/modules/ETV/Parser.ym"
+#line 127 "shared/perl/modules/ETV/Parser.ym"
 { my @formal_arg_names = ($ARG[1]); return \@formal_arg_names; }
 	],
 	[#Rule 19
 		 'arg_name', 1,
 sub
-#line 112 "shared/perl/modules/ETV/Parser.ym"
+#line 129 "shared/perl/modules/ETV/Parser.ym"
 { return $ARG[1]; }
 	],
 	[#Rule 20
@@ -810,7 +810,7 @@ sub
 	[#Rule 21
 		 'text', 1,
 sub
-#line 115 "shared/perl/modules/ETV/Parser.ym"
+#line 132 "shared/perl/modules/ETV/Parser.ym"
 { return $ARG[1]; }
 	]
 ],
@@ -818,7 +818,7 @@ sub
     bless($self,$class);
 }
 
-#line 117 "shared/perl/modules/ETV/Parser.ym"
+#line 134 "shared/perl/modules/ETV/Parser.ym"
 
 
 
