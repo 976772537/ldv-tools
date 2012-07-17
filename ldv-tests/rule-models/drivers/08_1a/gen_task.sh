23cf3a2db347b1091e38fc6c1c71b2bebebe8d2c
Eugene Novikov <joker@ispras.ru>
2012-07-17 19:31:43 +0400

    Name rule tests for model 08_1 properly (08_1a)

--- ldv-tests/rule-models/drivers/08_1a/gen_task.sh
+++ ldv-tests/rule-models/drivers/08_1a/gen_task.sh
@@ -1,3 +1,3 @@
 for NAME in `ls -d */ | xargs -l basename`; do 
-echo "driver=08_1--$NAME.tar.bz2;origin=external;kernel=linux-2.6.37;model=08_1;module=undefined.ko;main=undefined;verdict=safe"
+echo "driver=08_1a--$NAME.tar.bz2;origin=external;kernel=linux-2.6.37;model=08_1a;module=undefined.ko;main=undefined;verdict=safe"
 done;
