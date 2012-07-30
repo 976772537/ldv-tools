8b64a3c8d8dd7c3deb2635b540b38b7294565469
Marina Makienko <makienko@ispras.ru>
2012-07-30 11:58:44 +0400

    Add tests and regr-task for model 129_1a

---
+++ ldv-tests/rule-models/drivers/129_1a/test-S_arg/S_arg.c
@@ -0,0 +1,28 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/if_pppox.h>
+#include <linux/uaccess.h>
+
+struct my_device {
+	struct module mod;
+} mydev;
+
+static int __init my_init(void)
+{
+	const unsigned long *addr;
+	static int i;
+	i = find_next_zero_bit(addr, 5, 0);
+	return 0;
+}
+
+static void __exit my_exit(void)
+{
+}
+
+module_init(my_init);
+module_exit(my_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("LDV Project, Marina Makienko <makienko@ispras.ru>");
+MODULE_DESCRIPTION("Test");
+
