a25eff240719a9fcc1b3e297b6437ed43916f4df
Mikhail Mandrykin <mandrykin@ispras.ru>
2012-08-14 19:39:01 +0400

    Tests and tasks for 100_1a and 106_1a rule models; only tests for 132_1a and 133_1 models

---
+++ ldv-tests/rule-models/drivers/133_1/test-consume_skb/S-kfree_skb1-kfree_skb2.c
@@ -0,0 +1,18 @@
+/*
+ * Check if atomic_dec_and_mutex_lock is instrumented
+ */
+#include <linux/skbuff.h>
+
+int ldv_dummy_init(void)
+{
+	struct sk_buff *skb1, *skb2;
+	consume_skb(skb1);
+	consume_skb(skb2);
+	return 0;
+}
+
+int ldv_dummy_exit(void)
+{
+	return 0;
+}
+
