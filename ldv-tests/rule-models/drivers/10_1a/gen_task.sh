11167a4c92d29665ba4187ef17480c806d4cbf21
Vladimir Gratinskiy <gratinskiy@ispras.ru>
2012-08-23 14:04:08 +0400

    gen_task was changed

--- ldv-tests/rule-models/drivers/10_1a/gen_task.sh
+++ ldv-tests/rule-models/drivers/10_1a/gen_task.sh
@@ -1,3 +1,3 @@
 for NAME in `ls -d */ | xargs -l basename`; do 
-echo "driver=10_2a--$NAME.tar.bz2;origin=external;kernel=linux-3.5;model=10_1a;module=undefined.ko;main=undefined;verdict=safe"
+echo "driver=10_1a--$NAME.tar.bz2;origin=external;kernel=linux-3.5;model=10_1a;module=undefined.ko;main=undefined;verdict=safe"
 done;
--- ldv-tests/rule-models/drivers/111_1a/gen_task.sh
+++ ldv-tests/rule-models/drivers/111_1a/gen_task.sh
@@ -1,3 +1,3 @@
 for NAME in `ls -d */ | xargs -l basename`; do 
-echo "driver=111_2a--$NAME.tar.bz2;origin=external;kernel=linux-3.5;model=111_2a;module=undefined.ko;main=undefined;verdict=safe"
+echo "driver=111_1a--$NAME.tar.bz2;origin=external;kernel=linux-3.5;model=111_1a;module=undefined.ko;main=undefined;verdict=safe"
 done;
