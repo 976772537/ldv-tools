f396aba2445e89722ed92f70c9f4a8f74018dd26
Marina Makienko <makienko@ispras.ru>
2012-08-30 15:58:37 +0400

    Add tests for 110

---
+++ ldv-tests/rule-models/drivers/110_1a/test-spin_lock_bh/U-spin_lock_bh.c
@@ -0,0 +1,29 @@
+#include <linux/spinlock.h>
+#include "linux/module.h"
+#include "linux/kernel.h"
+
+struct my_device {
+	spinlock_t lock;
+};
+
+static int __init my_init(void)
+{
+	struct my_device *mydev;
+
+	spin_lock_bh(&mydev->lock);
+
+	return 0;
+}
+
+static void __exit my_exit(void)
+{
+	
+}
+
+module_init(my_init);
+module_exit(my_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("LDV Project, Marina Makienko <makienko@ispras.ru>");
+MODULE_DESCRIPTION("Test");
+
---
+++ ldv-tests/rule-models/drivers/110_1a/test-spin_lock_irq/U-spin_lock_irq.c
@@ -0,0 +1,30 @@
+#include <linux/spinlock.h>
+#include "linux/module.h"
+#include "linux/kernel.h"
+#include "media/videobuf-vmalloc.h"
+
+struct my_device {
+	spinlock_t lock;
+};
+
+static int __init my_init(void)
+{
+	struct my_device *mydev;	
+
+	spin_lock_irq(&mydev->lock);
+
+	return 0;
+}
+
+static void __exit my_exit(void)
+{
+	
+}
+
+module_init(my_init);
+module_exit(my_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("LDV Project, Marina Makienko <makienko@ispras.ru>");
+MODULE_DESCRIPTION("Test");
+
