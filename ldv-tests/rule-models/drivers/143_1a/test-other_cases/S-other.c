4f7139b3204c25be69c3f610b7016f7ad0e4b9ea
Vladimir Gratinskiy <gratinskiy@ispras.ru>
2012-09-18 17:02:33 +0400

    Add tests for rule 143

---
+++ ldv-tests/rule-models/drivers/143_1a/test-other_cases/S-other.c
@@ -0,0 +1,73 @@
+/*
+ * 
+ */
+#include <linux/module.h>
+#include <linux/major.h>
+#include <linux/fs.h>
+#include <linux/rwsem.h>
+
+static DECLARE_RWSEM(my_sem);
+
+static int n = 0;
+
+int my_func(void)
+{
+	return (n > 10) ? (-EINTR) : 0;
+}
+int misc_open(struct inode *inode, struct file *file)
+{
+	down_read(&my_sem);
+	down_read(&my_sem);
+	down_read(&my_sem);
+	down_write(&my_sem);
+	int res = my_func();
+	up_write(&my_sem);
+	up_read(&my_sem);
+	up_read(&my_sem);
+	up_read(&my_sem);
+	down_read(&my_sem);
+lab:
+	n += 1;
+	up_read(&my_sem);
+	if(n == 4)
+	{
+		down_read(&my_sem);
+		goto lab;
+	}
+	return res;
+}
+
+int misc_close(struct inode * inode, struct file * file)
+{
+	return 0;
+}
+static int misc_open_aux(struct inode *inode, struct file *file)
+{
+	return misc_open(inode, file);
+}
+static int misc_close_aux(struct inode *inode, struct file *file)
+{
+	return misc_close(inode, file);
+}
+
+static const struct file_operations misc_fops = {
+	.owner          = THIS_MODULE,
+	.open           = misc_open_aux,
+	.release        = misc_close_aux,
+};
+
+static int __init test_init(void)
+{
+	return register_chrdev(MISC_MAJOR, "misc", &misc_fops);
+}
+
+static void __exit test_exit(void)
+{
+  return;
+}
+
+module_init(test_init);
+module_exit(test_exit);
+
+MODULE_LICENSE("Apache 2.0");
+MODULE_AUTHOR("LDV Project, Vladimir Gratinskiy <gratinskiy@ispras.ru>");
