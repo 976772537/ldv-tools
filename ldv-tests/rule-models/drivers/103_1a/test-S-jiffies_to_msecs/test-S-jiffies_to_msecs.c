e88877675beedb2daeb314bf5c3673fbf1bfa047
Ilya Shchepetkov <shchepetkov@ispras.ru>
2012-09-25 17:27:05 +0400

    Added tests for 103_1a

---
+++ ldv-tests/rule-models/drivers/103_1a/test-S-jiffies_to_msecs/test-S-jiffies_to_msecs.c
@@ -0,0 +1,42 @@
+/** 
+  *  The test checks that correct jiffies_to_msecs is detected on the model 103_1a
+ **/
+#include <linux/module.h>
+#include <linux/major.h>
+#include <linux/fs.h>
+#include <linux/jiffies.h>
+#include <linux/delay.h>
+
+static int misc_open(struct inode *inode, struct file *file)
+{
+  unsigned long flags;
+  
+  msleep(jiffies_to_msecs(10));
+  
+  return 0; 	
+}
+
+static const struct file_operations misc_fops = {
+	.owner          = THIS_MODULE,
+	.open           = misc_open,
+};
+
+
+static int __init test_init(void)
+{
+  if (register_chrdev(MISC_MAJOR,"misc",&misc_fops))
+    goto fail_register;
+  return 0;
+	
+  fail_register:
+    return -1;
+}
+static void __exit test_exit(void)
+{
+}
+
+module_init(test_init);
+module_exit(test_exit);
+
+MODULE_LICENSE("Apache 2.0");
+MODULE_AUTHOR("LDV Project, Ilya Shchepetkov <shchepetkov@ispras.ru>");
