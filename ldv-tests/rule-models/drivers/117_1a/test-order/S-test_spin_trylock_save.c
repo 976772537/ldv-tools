c78cb798eed8630a35f9ed2db21ed38ae9324155
Vladimir Gratinskiy <gratinskiy@ispras.ru>
2013-02-25 13:12:24 +0400

    New tests for rule model 117_1a and fix in this model.

---
+++ ldv-tests/rule-models/drivers/117_1a/test-order/S-test_spin_trylock_save.c
@@ -0,0 +1,29 @@
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/version.h>
+
+MODULE_LICENSE( "GPL" );
+
+static int __init
+mod_init( void )
+{
+   unsigned long flags;
+   spinlock_t lock;
+
+   if(spin_trylock_irqsave(&lock, flags))
+   {
+      spin_unlock_irqrestore(&lock, flags);
+      return 0;
+   }
+   return 0;
+}
+
+static void __exit
+mod_exit( void )
+{
+}
+
+module_init( mod_init );
+module_exit( mod_exit );
+
