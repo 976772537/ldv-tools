8709a42923915937c340c058525c005317c9b50f
Eugene Novikov <joker@ispras.ru>
2013-11-12 12:17:40 +0400

    Added a C wrapper around ./install-ldv-cgroup
    Security policies most likely disable setuid for scripts (Perl, bash,
    etc.).

---
+++ res-manager/install-ldv-cgroup-wrapper.c
@@ -0,0 +1,38 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+#define INSTALL_LDV_CGROUP_SCRIPT  "./install-ldv-cgroup"
+
+
+int main()
+{
+  int ret;
+
+  if (setuid(0) == -1)
+  {
+    printf("Couldn't change user ID to root: %s\n", strerror(errno));
+    return errno;
+  }
+
+  if((ret = system(INSTALL_LDV_CGROUP_SCRIPT)) == -1)
+  {
+    printf("Couldn't execute" INSTALL_LDV_CGROUP_SCRIPT ": %s\n", strerror(errno));
+    return errno;
+  }
+
+  if (WEXITSTATUS(ret))
+  {
+    printf("Something went wrong\n");
+    return WEXITSTATUS(ret);
+  }
+
+  printf("LDV control group was likely installed successfully\n");
+
+  return 0;
+}
