4c2f4265095a427ec41bc01c72e5d910c0b0c59a
Vitaly Mordan <mordan@ispras.ru>
2014-11-18 12:40:27 +0300

    Revert "Resolved an issue with adjusting error trace height."
    This reverts commit 74028eb63a1f2d393b3918bded8a3dee2ffe8071.
    
    Conflicts:
    
    	etv/js/etv.js

--- etv/js/etv.js
+++ etv/js/etv.js
@@ -64,21 +64,9 @@ function resizeTabWidths(isResize) {
   }
 }
 
-// Resize to fit the available height.
-function resizeHeight(isResize) {
-  var resizedHeight = window.screen.availHeight * 0.65;
-  $('#ETVErrorTrace').height(resizedHeight + 'px');
-  $('.ETVSrc').height(resizedHeight + 'px');
-}
-
 $(document).ready(function() {
   resizeTabWidths(false);
 });
-
-$(document).ready(function() {
-  resizeHeight(false);
-});
-
 
 // Allow to choose a current tab.
 $(document).ready(function() {
