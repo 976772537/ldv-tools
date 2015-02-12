d814423a845d7d90c6b41ca73458aa8ab626cf6e
Vadim Mutilin <mutilin@ispras.ru>
2015-02-12 11:20:16 +0300

    Add tests for 153_1a prepared by Kirill Smorodin

---
+++ ldv-tests/rule-models/drivers/153_1a/test-idr/U-struct-destroy.c
@@ -0,0 +1,32 @@
+/*
+ * Check correct methods call for model 777.
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/idr.h>
+#include <linux/slab.h>
+
+struct A{
+	struct idr idp;
+};
+
+static int test_driver(void)
+{
+	struct A *a = kmalloc(sizeof(struct A), GFP_KERNEL);
+
+	idr_destroy(&a->idp);
+
+	return 0;
+}
+
+static int __init my_init(void)
+{
+	int ret_val = test_driver();
+	return ret_val;
+}
+
+module_init(my_init);
+
+
+
