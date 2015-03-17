8507c9d2b495b819cc20599ec09fadbcf0c4c45f
Vladimir Gratinskiy <gratinskiy@ispras.ru>
2015-03-17 12:41:39 +0300

    New feature.

--- test.py
+++ test.py
@@ -1,8 +1,12 @@
 target = open('test.txt', 'r')
 in_data = target.read()
+
 if in_data == 'Some text\n':
     print 'OK'
     exit(0)
 else:
     print 'FAIL'
     exit(1)
+if 2 > 1:
+    print 'OK'
+    exit(0)
