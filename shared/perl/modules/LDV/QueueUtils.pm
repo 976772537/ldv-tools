27286b6f1ea69c86aa2f6077f6f211001bb6188c
Ilja Zakharov <ilja.zakharov@ispras.ru>
2015-03-23 19:19:49 +0400

    Start implementation of queue based on files instead of SOAP
    Implement saving commands into data files in collection and addition
    of short messages into index file which should be used for navigation
    to bigger data files. Implenebtation is placed as shared perl lib.
    It is implemented in bce for command saving and ldv-core just
    for queue initialization.

---
+++ shared/perl/modules/LDV/QueueUtils.pm
@@ -0,0 +1,106 @@
+# Package for replacement SOAP::Lite. Implement data exchanging within files.
+package LDV::QueueUtils;
+
+use File::Basename;
+use strict;
+use warnings;
+use File::Path qw(make_path remove_tree);
+use Fcntl qw(:flock SEEK_END);
+
+use vars qw(@ISA @EXPORT_OK @EXPORT);
+@EXPORT=qw( init_queue post_message post_data_message);
+use base qw(Exporter);
+
+# Initialize queue
+sub init_queue {
+	my ($collection, $index) = @_;
+
+	# Recreate collection
+	if (-d $collection) {
+		remove_tree $collection;
+	}
+	make_path $collection;
+
+	# Recreate index file
+	if (-f $index) {
+		remove_tree $index;
+	}
+	open my $fh, '>', $index or die "Cannot open index file '$index'";
+	close $fh;
+
+	return;
+}
+
+# Add new command to both collection and message to index file 
+sub post_message {
+	my ($index, $type, $message) = @_;
+
+	if ($type =~ /[\n|::]/g || $message =~ /[\n|::]/g) {
+		die "One can post only one-line messages without '::' to the index of the queue";
+	}
+	else {
+		# Open
+		open my $fh, '>>', $index or die "Cannot open index file '$index': $!";
+		flock($fh, LOCK_EX) or die "Cannot lock index file '$index': $!";
+
+		# Print
+		print {$fh} $type . q{::} . $message . "\n" 
+			or die "Cannot print index file '$index': $!";
+		
+		# Close
+		flock($fh, LOCK_UN) or die "Cannot unlock index file '$index': $!";
+		close $fh or die "Cannot close index file '$index': $!";
+	}
+
+	return;
+}
+
+sub post_data_message {
+	my ($collection, $index, $data_file, $type, $data) = @_;
+
+	my $file = "$collection/$data_file";
+	
+	# Prepare path
+	my $path = dirname($file);
+	if (!-d $path) {
+		make_path $path;
+	}
+
+	# Post data
+	open my $fh, '>', $file or die "Cannot open data file '$file': $!";
+	flock($fh, LOCK_EX) or die "Cannot lock data file '$file': $!";
+	print {$fh} $data or die "Cannot print to data file '$file': $!";
+
+	# Close
+	flock($fh, LOCK_UN) or die "Cannot unlock data file '$file': $!";
+	close $fh or die "Cannot close data file '$file': $!";
+
+	# post short message
+	post_message($index, $type, "$collection/$data_file");
+}
+
+# Add special message which ends collecting
+sub finish_collecting {
+	return;
+}
+
+# Add artificial LDM command
+sub add_ldm_command {
+	return;
+}
+
+# Check that command collecting is finished
+sub is_finished {
+	return;
+}
+
+# Add new command or message to index file
+sub __post_to_index {
+	return;
+}
+
+# Add coomand to the collection
+sub __save_to_collection {
+	return;
+}
+ 
