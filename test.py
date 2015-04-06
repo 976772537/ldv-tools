f9a974f6336dd202ab6cb058b610d42fcaaa98b2
Vladimir Gratinskiy <gratinskiy@ispras.ru>
2015-04-06 18:58:42 +0300

    Delete smth files and add time to test.py

--- test.py
+++ test.py
@@ -1,13 +1,21 @@
 import time
+import datetime
 
 target = open('test.txt', 'r')
 
-in_data = target.read()
-time.sleep(300)
+in_data = target.readline()
+time.sleep(1)
+target.close()
+target = open('test.txt', 'w')
+
+cur_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+line1 = "Time of testing: %s!" % cur_time
+target.write("%s%s" % (in_data, line1))
 
 if in_data == 'Some text\n':
-    print 'OK'
+    print cur_time + ': OK'
     exit(0)
 else:
-    print 'FAIL'
+    print cur_time + ': FAIL'
     exit(1)
